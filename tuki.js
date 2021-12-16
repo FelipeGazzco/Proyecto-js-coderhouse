e355483c8da93234a324252e0eebe271f3e5fdae
Felipe Gazcon <pipegazcon123@gmail.com>
2021-12-16 19:12:55 -0300

    Guardado 1 16 de Diciembre

--- tuki.js
+++ tuki.js
@@ -2,18 +2,20 @@ let array1 = [];
 let array2 = [];
 
 class Personaje{
-    constructor(nombre, origen, ocupacion){
+    constructor(nombre, origen, ocupacion, costo){
         this.nombre = nombre;
         this.origen = origen;
         this.ocupacion = ocupacion;
+        this.costo = costo
     }
 }
 
 class Item{
-    constructor(nombre2, origen2, uso){
+    constructor(nombre2, origen2, uso, costo2){
         this.nombre = nombre2
         this.origen = origen2
         this.uso = uso
+        this.costo = costo2
     }
 }
 
@@ -22,13 +24,14 @@ boton.onclick = () => {
     let uno = document.getElementById("nombre").value;
     let dos = document.getElementById("origen").value;
     let tres = document.getElementById("ocupacion").value;
+    let cuatro = document.getElementById("costo").value;
     if (localStorage.getItem("arrayGuardado") === null) {
         console.log("Almacenaje vacio");
     }else{
         array1 = JSON.parse(localStorage.getItem("arrayGuardado"));
         console.log("+1");
     }
-    array1.push(new Personaje(uno, dos, tres));
+    array1.push(new Personaje(uno, dos, tres, cuatro));
     localStorage.setItem("arrayGuardado", JSON.stringify(array1));
     console.log(array1);
     document.location.reload()
@@ -39,20 +42,25 @@ boton1.onclick = () => {
     let uno = document.getElementById("nombre2").value;
     let dos = document.getElementById("origen2").value;
     let tres = document.getElementById("uso").value;
+    let cuatro = document.getElementById("costo2").value;
     if (localStorage.getItem("arrayGuardado2") === null) {
         console.log("Almacenaje vacio");
     }else{
         array2 = JSON.parse(localStorage.getItem("arrayGuardado2"));
         console.log("+1");
     }
-    array2.push(new Item(uno, dos, tres));
+    array2.push(new Item(uno, dos, tres, cuatro));
     localStorage.setItem("arrayGuardado2", JSON.stringify(array2));
     console.log(array2);
     document.location.reload()
 };
 
-let boton2 = document.getElementById("intento1");
-boton2.onclick = () => {
-    $(".navigator").fadeIn(setTimeout(function(){$(".navigator").fadeOut()}, 4000));
-    $("#intento1").hide(setTimeout(function(){$("#intento1").fadeIn()}, 4300));
-}
+$("#intento1").click(function() {
+
+    $("#intento1").fadeOut("slow", function() {
+   
+     $(".navigator").fadeIn("slow");
+   
+    });
+   
+   })
--- tuki2.js
+++ tuki2.js
@@ -5,7 +5,8 @@ if (localStorage.getItem("arrayGuardado") === null) {
     for (const Personaje of array1) {
         $(".listaColeccion").append(`<li><h3> ${Personaje.nombre}</h3>
                   <p> Origen: ${Personaje.origen}</p>
-                  <p> Ocupacion: ${Personaje.ocupacion}</p></li>`);
+                  <p> Ocupacion: ${Personaje.ocupacion}</p>
+                  <p> Costo: ${Personaje.costo}<p></li>`);
     }
 }
 
@@ -16,13 +17,23 @@ if (localStorage.getItem("arrayGuardado2") === null) {
     for (const Item of array2) {
         $(".listaColeccion2").append(`<li><h3> ${Item.nombre}</h3>
                   <p>  Origen: ${Item.origen}</p>
-                  <p> Uso: ${Item.uso}</p></li>`);
+                  <p> Uso: ${Item.uso}</p>
+                  <p> Costo: ${Item.costo}<p></li>`);
     }
 }
 
-let boton2 = document.getElementById("intento1");
+/*let boton2 = document.getElementById("intento1");
 boton2.onclick = () => {
     $(".navigator").fadeIn(setTimeout(function(){$(".navigator").fadeOut()}, 4000));
     $("#intento1").hide(setTimeout(function(){$("#intento1").fadeIn()}, 4300));
-}
+}*/
 
+$("#intento1").click(function() {
+
+    $("#intento1").fadeOut("slow", function() {
+   
+     $(".navigator").fadeIn("slow");
+   
+    });
+   
+   })
