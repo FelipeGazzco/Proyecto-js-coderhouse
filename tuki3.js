d47bb40ce11b668ab690a7504c6bd4a958797a7f
Felipe Gazcon <pipegazcon123@gmail.com>
2021-12-21 23:31:18 -0300

    Gurdado 1 21 de Diciembre

--- tuki3.js
+++ tuki3.js
@@ -1,12 +1,7 @@
-let plata = localStorage.getItem("plataGuardada");
+let plata3 = localStorage.getItem("plataGuardada");
 
 $("#statik").append(`<div><h3>Plata a disposicion</h3>
-                  <p>Actual: ${plata}</p></div>`);
-
-
-
-
-
+                  <p>Actual: ${plata3 * 100}</p></div>`);
 
 $("#intento1").click(function() {
 
@@ -16,4 +11,46 @@ $("#intento1").click(function() {
    
     });
    
+})
+
+const URL_PERSONAJES = "https://rickandmortyapi.com/api/character";
+
+$.get(URL_PERSONAJES, (data, status) => {
+
+    let resultados = data.results;
+
+    let count = 0;
+    for(const personaje of resultados) {
+        count++;
+        $(".tiendaDiv").append(`
+            <div>
+                <p>Nombre: ${personaje.name}</p>
+                <img src="${personaje.image}" width="100px">
+
+            </div>
+        `)
+    }
+
+})
+
+const URL_PERSONAJES2 = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20"
+
+$.get(URL_PERSONAJES2, (data, status) => {
+
+    let resultados = data.results;
+
+    for(const pokemon of resultados) {
+        $.get(pokemon.url, (data_poke, status) => {
+            let img_main = data_poke.sprites.other["official-artwork"].front_default
+            $(".tiendaDiv2").append(`
+                <div>
+                    <p>Nombre: ${pokemon.name}</p>
+                    <img src="${img_main}" width="100px">
+                </div>
+            `)
+        })
+
+
+    }
+
 })
