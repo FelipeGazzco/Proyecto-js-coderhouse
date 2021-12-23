0b05e5179a31bc69fcc2164e06ecdaf5f39ed8f7
Felipe Gazcon <pipegazcon123@gmail.com>
2021-12-23 17:32:46 -0300

    Guardado 1 23 de Diciembre

--- tuki3.js
+++ tuki3.js
@@ -1,37 +1,32 @@
-let plata3 = localStorage.getItem("plataGuardada");
-
-$("#statik").append(`<div><h3>Plata a disposicion</h3>
-                  <p>Actual: ${plata3 * 100}</p></div>`);
-
-$("#intento1").click(function() {
-
-    $("#intento1").fadeOut("slow", function() {
-   
-     $(".navigator").fadeIn("slow");
-   
-    });
-   
-})
 
 const URL_PERSONAJES = "https://rickandmortyapi.com/api/character";
 
 $.get(URL_PERSONAJES, (data, status) => {
 
     let resultados = data.results;
-
-    let count = 0;
     for(const personaje of resultados) {
-        count++;
         $(".tiendaDiv").append(`
             <div>
                 <p>Nombre: ${personaje.name}</p>
                 <img src="${personaje.image}" width="100px">
-
+                <p>Clase: Personaje </p>
+                <button class="consume">Comprar (250)</button>
+                <script>
+                function consume(){
+                    let abcd = localStorage.getItem("plataGuardada");
+                    if (abcd >= 2.5) {
+                        let abcd12 = abcd -2.5;
+                        localStorage.setItem("plataGuardada", abcd12);
+                    }else{
+                        alert("Imposible")
+                    }
+                    document.ocation.reload();
+                }
+            </script>
             </div>
         `)
     }
-
-})
+});
 
 const URL_PERSONAJES2 = "https://pokeapi.co/api/v2/pokemon?offset=20&limit=20"
 
@@ -46,11 +41,45 @@ $.get(URL_PERSONAJES2, (data, status) => {
                 <div>
                     <p>Nombre: ${pokemon.name}</p>
                     <img src="${img_main}" width="100px">
+                    <p>Clase: Personaje </p>
+                    <button class="consume2" onclick="consume2()">Comprar (350)</button>
+                    <script>
+                        function consume2(){
+                            let abcd2 = localStorage.getItem("plataGuardada");
+                            if (abcd2 >= 3.5) {
+                                let abcd22 = abcd2 -3.5;
+                                localStorage.setItem("plataGuardada", abcd22);
+                            }else{
+                                alert("Imposible")
+                            }
+                            document.ocation.reload();
+                        }
+                    </script>
                 </div>
             `)
-        })
-
-
+        });
     }
+});
 
+function consume2() {
+    alert('Se ha dado clic al botón!');
+    let abcd2 = localStorage.getItem("plataGuardada");
+    if (abcd2 >= 3.5) {
+        let abcd22 = abcd2 -3.5;
+        localStorage.setItem("plataGuardada", abcd22);
+    }else{
+        alert("Imposible")
+    }
+    documen.location.reload();
+}
+
+let plata3 = localStorage.getItem("plataGuardada");
+
+$("#statik").append(`<div><h3>Plata a disposicion</h3>
+                  <p>Actual: ${plata3 * 100}</p></div>`);
+
+$("#intento1").click(function() {
+    $("#intento1").fadeOut("slow", function() {
+     $(".navigator").fadeIn("slow");   
+    });  
 })
